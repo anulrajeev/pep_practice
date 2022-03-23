e218e4f1b77c6938770b2e9ed0975910a4f9559f
anulrajeev <anulrajeev@gmail.com>
2022-03-23 18:36:41 +0530

    first commit

---
+++ leaderBoard.js
@@ -0,0 +1,21 @@
+let request = require('request');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+let link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
+request(link, cb);
+
+function cb(err, res, body)
+{
+    if(err)
+        console.log(err);
+    else
+    {
+        const dom = new JSDOM(body);
+        let scorecardLink = dom.window.document.querySelectorAll('a[data-hover="Scorecard"]');
+        for(let i=0;i<scorecardLink.length;i++)
+            {
+                console.log("https://www.espncricinfo.com/" +  scorecardLink[i].href);
+            }
+    }
+}
